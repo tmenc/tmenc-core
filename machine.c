4f5eca8bf583e3cdea8588b25558c4d01d362794
tmenc <isadora.sosa@protonmail.com>
2020-02-13 22:26:15 +0100

    fixup

--- machine.c
+++ machine.c
@@ -7,15 +7,24 @@ typedef uint32_t itype;
 itype simple(itype x) {
 	itype a = 1664525;
 	itype b = 1013904223;
-	return (x * a + b);
+	return x * a + b;
+}
+
+itype bitn(itype x, int n) {
+	return (x >> n) & 1;
+}
+
+itype to1bit(itype x) {
+	if (x > 2147483648) { return 1; }
+	else { return 0; }
 }
 
 void main() {
 	itype x = 200;
 
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < 100; i++) {
 		x = simple(x);
-		printf("%u\n", x);
+		printf("%u\n", to1bit(x));
 	}
 }
 
