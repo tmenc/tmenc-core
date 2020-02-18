0d69ed8ed5a127159de5ced446a6b103a52027c9
tmenc <isadora.sosa@protonmail.com>
2020-02-18 16:13:15 +0100

    save@emacs

--- machine.js
+++ machine.js
@@ -197,7 +197,7 @@ function make_tm(machine_bits, address_size, weak_rng) {
 
 		const wt_bit = read_chosen_bit(shift);
 		const rt_direction_bit = read_chosen_bit(shift);
-		const wt_direction_bit = read_n_collapse(2, 3, shift);
+		const wt_direction_bit = read_n_collapse(0, 1, shift);
 
 		avg = ((avg * count) + wt_direction_bit) / (count + 1)
 		count++;
