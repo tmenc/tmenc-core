dbfc6424332421fb90923b346673c6c3457f74ef
tmenc <isadora.sosa@protonmail.com>
2020-05-12 11:48:35 +0200

    save@emacs

--- cli.js
+++ cli.js
@@ -11,7 +11,7 @@ rl.question('pass: ', (pass) => {
 	rl.question("file: ", (file) => {
 
 		var file_buffer = fs.readFileSync(file);
-		var file_stream = buffer_to_byte_stream(file_buffer);
+		var file_stream = byte_stream_to_binary_stream(buffer_to_byte_stream(file_buffer));
 		var file_vector = stream_to_vector(file_stream);
 		console.log("len:", file_vector.length, "bytes:", file_vector);
 		// var nums = ascii_to_binary(pass);
