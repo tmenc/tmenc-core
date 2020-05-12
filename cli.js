a2c2a0f9e1075979c746eb91f183b9c559b9f82e
tmenc <isadora.sosa@protonmail.com>
2020-05-12 16:06:40 +0200

    save@emacs

--- cli.js
+++ cli.js
@@ -10,7 +10,7 @@ var rl = readline.createInterface({
 
 rl.question('pass: ', (pass) => {
 	rl.question("file: ", (file) => {
-		rl.question("salt: " (salt) => {
+		rl.question("salt: ", (salt) => {
 
 			var file_buffer = fs.readFileSync(file);
 			var file_stream = byte_stream_to_binary_stream(buffer_to_byte_stream(file_buffer));
@@ -18,7 +18,8 @@ rl.question('pass: ', (pass) => {
 			var salt_stream = ascii_to_binary_stream(ascii); // usually get from prev version
 
 			var combined = append_streams([salt_stream, pass_stream, file_stream]);
-			
+
+			console.log('vec:', stream_to_vector(combined));
 
 			rl.close();
 		});
