1467aacb5aa3c1ac8c620aa07951c1d6e66dad44
tmenc <isadora.sosa@protonmail.com>
2020-05-12 16:34:46 +0200

    save@emacs

--- cli.js
+++ cli.js
@@ -8,6 +8,10 @@ var rl = readline.createInterface({
 	terminal: false,
 });
 
+function debug_vec(v) {
+	console.log('len(v):', v.length, 'v:', v);
+}
+
 rl.question('pass: ', (pass) => {
 	rl.question("file: ", (file) => {
 		rl.question("salt: ", (salt) => {
@@ -17,9 +21,9 @@ rl.question('pass: ', (pass) => {
 			var pass_stream = ascii_to_binary_stream(pass);
 			var salt_stream = ascii_to_binary_stream(salt); // usually get from prev version
 
-			var combined = append_streams([salt_stream, pass_stream, file_stream]);
+			// var combined = append_streams([salt_stream, pass_stream, file_stream]);
 
-			console.log('vec:', stream_to_vector(file_stream));
+			debug_vec(stream_to_vector(file_stream));
 
 			rl.close();
 		});
