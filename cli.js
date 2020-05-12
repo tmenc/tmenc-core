99087ba4556aa7376b273c57ee9725905e601aa1
tmenc <isadora.sosa@protonmail.com>
2020-05-12 16:13:21 +0200

    save@emacs

--- cli.js
+++ cli.js
@@ -14,8 +14,8 @@ rl.question('pass: ', (pass) => {
 
 			var file_buffer = fs.readFileSync(file);
 			var file_stream = byte_stream_to_binary_stream(buffer_to_byte_stream(file_buffer));
-			var pass_stream = ascii_to_binary_stream(ascii);
-			var salt_stream = ascii_to_binary_stream(ascii); // usually get from prev version
+			var pass_stream = ascii_to_binary_stream(pass);
+			var salt_stream = ascii_to_binary_stream(salt); // usually get from prev version
 
 			var combined = append_streams([salt_stream, pass_stream, file_stream]);
 
