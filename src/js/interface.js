2a4025e2152c2c8044e21ae6d02b361796e24c9e
tmenc <isadora.sosa@protonmail.com>
2020-05-16 13:42:16 +0200

    save@emacs

--- src/js/interface.js
+++ src/js/interface.js
@@ -1,17 +1,17 @@
 
-// NOTE: passes should depend on `length(pass++file)'
-function make_key(pass, salt, file, size, passes) {
+// NOTE: `wrap_count' should depend on `length(pass++file)'
+function make_key(pass, machine_bits_hex, file, size, wrap_count) {
 	var file_buffer = fs.readFileSync(file);
 	var file_stream = byte_stream_to_binary_stream(buffer_to_byte_stream(file_buffer));
 	var pass_stream = hex_to_binary_stream(pass);
-	var salt_stream = hex_to_binary_stream(salt); // usually get from prev version
+	var mach_stream = hex_to_binary_stream(machine_bits_hex); // usually get from prev version
 
-	var combined_input = append_streams([pass_stream, file_stream]);
+	var input_stream = append_streams([pass_stream, file_stream]);
 
-	var machine_bits = stream_to_bitarr(salt);
-	var input_bits = stream_to_bitarr(combined_input);
+	var machine_bits = stream_to_bitarr(mach_stream);
+	var input_bits = stream_to_bitarr(input_stream);
 	var env = make_tm_env(machine_bits, input_bits, size);
-	tm_run_for_wc(env, passes);
+	tm_run_for_wc(env, wrap_count);
 
 	return env.write_tape;
 }
