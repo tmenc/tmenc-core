5ea6287ec5dbc566279db90019b78966fc555c39
tmenc <isadora.sosa@protonmail.com>
2020-05-16 20:55:58 +0200

    save@emacs

--- src/js/cli.js
+++ src/js/cli.js
@@ -17,13 +17,21 @@ rl.question('pass: ', (pass) => {
 		rl.question("keyfile: ", (file) => {
 			rl.question("machine-size: ", (machine_size_s) => {
 				var machine_size = parseInt(machine_size_s);
-				rl.question("key-size: ", (key_size_s) => {
-					var key_size = parseInt(key_size_s);
 					rl.question("wrap-count: ", (wrap_count_s) => {
 						var wrap_count = parseInt(wrap_count_s);
+						rl.question("input-file: ", (input_file_path) => {
 
-						var key = make_key(pass, salt, file, key_size, machine_size, wrap_count);
-						debug_vec(key)
+							var input_file_stream = byte_stream_to_binary_stream(buffer_to_byte_stream(fs.readFileSync(input_file_path)));
+							var input_file_bits = stream_to_bitarr(input_file_stream);
+							var key_size = bitarray_length(input_file_bits);
+
+							var key = make_key(pass, salt, file, key_size, machine_size, wrap_count);
+							debug_vec(key)
+
+							for (var i = 0; i < key_size; i++) {
+								var x = key[i] ^ input_file_bits[i];
+								process.stdout.write(x);
+							}
 
 						rl.close();
 					});
