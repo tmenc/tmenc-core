f1bd208f8b49ec02ecb05f47d1a505c7ca94ea72
tmenc <isadora.sosa@protonmail.com>
2020-05-18 15:42:23 +0200

    save@emacs

--- test/test-util.h
+++ test/test-util.h
@@ -47,34 +47,40 @@ void bitarray_print(bitarr arr) {
 	printf("]\n");
 }
 
-void test_bit_ops() {
-	size_t len = 10;
-	bitarr arr = bitarray_alloc(len);
+#define TEST_BIT_OPS_LEN 10
 
-	assert(len == bitarray_length(arr));
+void test_bit_ops() {
+	size_t i;
+	bitarr arr = bitarray_alloc(TEST_BIT_OPS_LEN);
+	bit answers[TEST_BIT_OPS_LEN];
+
+	assert(TEST_BIT_OPS_LEN == bitarray_length(arr));
 
 	bitarray_zero_out(arr);
+	for (i = 0; i < TEST_BIT_OPS_LEN; i++) {
+		answers[i] = 0;
+	}
 
 	bitarray_print(arr);
-	assert(arr.buffer[0] == 0 && arr.buffer[1] == 0);
-
 	bitarray_set_bit(arr, 0, 1);
+	answers[0] = 1;
 	bitarray_print(arr);
-	assert(arr.buffer[0] == 1 && arr.buffer[1] == 0);
-
 	bitarray_set_bit(arr, 1, 1);
+	answers[1] = 1;
 	bitarray_print(arr);
-	assert(arr.buffer[0] == 3 && arr.buffer[1] == 0);
-
 	bitarray_set_bit(arr, 2, 1);
+	answers[2] = 1;
 	bitarray_print(arr);
-	assert(arr.buffer[0] == 7 && arr.buffer[1] == 0);
-
 	bitarray_set_bit(arr, 7, 1);
+	answers[7] = 1;
 	bitarray_print(arr);
-	assert(arr.buffer[0] == 135 && arr.buffer[1] == 0);
-
 	bitarray_set_bit(arr, 9, 1);
+	answers[9] = 1;
 	bitarray_print(arr);
-	assert(arr.buffer[0] == 135 && arr.buffer[1] == 2);
+
+	bitarray_zero_out(arr);
+	for (i = 0; i < TEST_BIT_OPS_LEN; i++) {
+		assert(answers[i] == bitarray_at(arr, i));
+	}
 }
+
