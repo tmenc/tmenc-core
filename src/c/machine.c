b1c9de4878181bd9622c7dc588602fa381a57dea
tmenc <isadora.sosa@protonmail.com>
2020-05-19 19:04:45 +0200

    save@emacs

--- src/c/machine.c
+++ src/c/machine.c
@@ -275,8 +275,6 @@ struct tm_env_s {
 };
 typedef struct tm_env_s tm_env;
 
-int counter = 0;
-
 static opaque
 tm_env_generator(void *state, bit *finished_q) {
 	tm_env *env = state;
@@ -287,11 +285,6 @@ tm_env_generator(void *state, bit *finished_q) {
 	size_t new_register_value;
 
 	while (1) {
-
-		counter++;
-		if (counter % 1000 == 0) {
-			fprintf(stderr, "WTF %d\n", counter);
-		}
 
 		read_tape_bit = stream_read(env->input_stream).binary;
 #ifdef DEBUG
