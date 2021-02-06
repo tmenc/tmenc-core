9b52305fe7008fe8d37496b787163d54a89e1484
tmenc <isadora.sosa@protonmail.com>
2021-02-06 15:43:08 +0100

    start c cli

--- src/c/cli.c
+++ src/c/cli.c
@@ -1,16 +1,62 @@
+
 #include <stdio.h>
+#include <string.h>
+
+#define BUFFER_LEN 512
+
+/* 0 - unknown
+ * 1 - encrypt
+ * 2 - decrypt */
+int mode = 0;
+
+static
+fail(void) {
+	exit(1);
+}
+
+static
+int string_equal_p(char *a, char *b) {
+	return (strcmp(a, b) == 0);
+}
+
+static
+void decrypt_file(void) {
+	/* TODO */
+}
+
+static
+void encrypt_file(void) {
+	
+}
+
+static
+void set_mode(void) {
+	char answer[BUFFER_LEN];
+	printf("entrypt/decrypt: ");
+	fgets(answer, BUFFER_LEN, stdin);
+
+	if (string_equal_p(answer, "encrypt")) {
+		mode = 1;
+	} else if (string_equal_p(answer, "decrypt")) {
+		mode = 2;
+	} else {
+		fprintf(stderr, "Expected encrypt or decrypt, got %s\n", answer);
+		fail();
+	}
+}
+
+static
+void entry(void) {
+	set_mode();
+	if (mode == 1) {
+		encrypt_file();
+	} else {
+		decrypt_file();
+	}
+}
 
 int main(int argc, char **argv) {
-	char passbuf[512];
-	char filebuf[512];
-
-	printf(">pass: ");
-	scanf("%s", passbuf);
-
-	printf(">file: ");
-	scanf("%s", filebuf);
-
-	printf("we got pass: '%s', file: '%s'\n", passbuf, filebuf);
+	entry();
 	return 0;
 }
 
