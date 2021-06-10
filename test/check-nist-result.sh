074980684321edf3b690cb0262f1ada001565952
tmenc <isadora.sosa@protonmail.com>
2021-06-10 18:47:37 +0200

    small fixes

--- test/check-nist-result.sh
+++ test/check-nist-result.sh
@@ -4,7 +4,7 @@ MAX="$1"
 FILE="$2"
 CUR=$(cat "$FILE" | grep 'NUMBER OF FAILS' | grep '[0-9]*' -o | head -n 1)
 
-if [ $CUR -le $MAX ]
+if test "$CUR" -le "$MAX"
 then exit 0
 else
 	echo "TOO MUCH FAILS: $CUR%"
