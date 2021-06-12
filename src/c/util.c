27372299db7db9961cadb4cc9fbbbd850ad722b6
tmenc <isadora.sosa@protonmail.com>
2021-06-12 10:42:57 +0200

    save

--- src/c/util.c
+++ src/c/util.c
@@ -886,6 +886,17 @@ tm_get_stream_bitarr(stream *s, size_t input_wrap_count, size_t input_size, size
 static char
 normalize_text_char(char c) {
 	switch (c) {
+		case '1': return '1';
+		case '2': return '2';
+		case '3': return '3';
+		case '4': return '4';
+		case '5': return '5';
+		case '6': return '6';
+		case '7': return '7';
+		case '8': return '8';
+		case '9': return '9';
+		case '0': return '0';
+
 		case 'a': return 'a';
 		case 'b': return 'b';
 		case 'c': return 'c';
--- src/js/util.js
+++ src/js/util.js
@@ -344,6 +344,17 @@ function byte_stream_to_byte_buffer(stream) {
 
 function normalize_text_char(c) {
 	switch (c) {
+		case '1': return '1';
+		case '2': return '2';
+		case '3': return '3';
+		case '4': return '4';
+		case '5': return '5';
+		case '6': return '6';
+		case '7': return '7';
+		case '8': return '8';
+		case '9': return '9';
+		case '0': return '0';
+
 		case 'a': return 'a';
 		case 'b': return 'b';
 		case 'c': return 'c';
